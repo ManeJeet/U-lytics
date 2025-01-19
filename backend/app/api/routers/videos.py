bcc2a37acfa50de98eaf40d434b1d321f9a0fdaf
Seyam Omar <omarseyam1729@gmail.com>
2025-01-20 01:41:05 +1100

    test commit

--- backend/app/api/routers/videos.py
+++ backend/app/api/routers/videos.py
@@ -6,4 +6,6 @@ router = APIRouter()
 @router.get("/{video_id}")
 async def get_video_details(video_id: str):
     youtube_service = YouTubeService()
+    print("hello world")
     return youtube_service.get_video_details(video_id)
+
